d2a66b3b66b6e86962d8c7e1f1201927378e2e21
Brian Button <brian.button@centurylinkcloud.com>
2015-09-21 17:48:17 -0500

    wip commit - trying to figure out why it doesn't work

--- board_cleaner.rb
+++ board_cleaner.rb
@@ -2,24 +2,45 @@ require 'orchestrate'
 require 'JSON'
 
 class BoardCleaner
+  def initialize
+    @client = Orchestrate::Client.new(ENV["ORCHESTRATE_API_KEY"], ENV["ORCHESTRATE_ENDPOINT"])
+  end
+
   def run board_id
-    app = Orchestrate::Application.new(ENV["ORCHESTRATE_API_KEY"], ENV["ORCHESTRATE_ENDPOINT"])
-    trello_data = app[ENV["ORCHESTRATE_COLLECTION"]]
-
-    query_string = "value.data.board.id:#{board_id}"
-
-    complete_actions = trello_data.search("#{query_string}").order(:card_id, :asc, :date, :asc, ).kinds('event').find
+    complete_results = []
+    offset = 0
+    begin
+      results = do_search(board_id, offset)
+      complete_results.concat(results)
+      offset += 100
+    end until results.count == 0
 
     action_ids = []
-    complete_actions.each do |score, action|
-      action_ids << {key: action["card_id"], type: "card_actions", timestamp: nil, ordinal: action.ordinal}
+    complete_results.each do |action|
+      action_ids << {key: action["path"]["key"], type: "card_actions", timestamp: action["path"]["timestamp"], ordinal: action["path"]["ordinal"]}
     end
     action_ids.uniq!
 
+    counter = 0
     action_ids.each do |a|
-      trello_data.purge_event("TrelloData", a[:key], a[:type], a[:timestamp], a[:ordinal])
+      puts "purging: TrelloData:#{a[:key]}:#{a[:type]}:#{a[:timestamp]}:#{a[:ordinal]}"
+      @client.purge_event("TrelloData", a[:key], a[:type], a[:timestamp], a[:ordinal])
+      counter += 1
+      puts "#{counter}"
     end
   end
+
+  def do_search board_id, offset
+    query_string = "value.data.board.id:#{board_id}"
+    params = {
+        sort: 'card_id:asc, date:asc',
+        limit: 100,
+        offset: offset
+    }
+    complete_actions = @client.search(:TrelloData, "@path.kind:event AND #{query_string}", params)
+    complete_actions.results
+  end
+
 end
 
 board_cleaner = BoardCleaner.new
